1ab08d09bd23ab7e5d43d9720cdf6996e62e97c8
Aaron Brown <a.h.brown@warwick.ac.uk>
2012-11-13 11:52:53 +1100

    Charge residues
    Added a couple extra critieria to make output more managable

--- charge_dist.pl
+++ charge_dist.pl
@@ -23,11 +23,11 @@ while ($file = readdir(DH)) {
     while ($line = readline(FH)) {
         $line =~ s/^\s+//;
         @params = split(/\s+/, $line);
-		if($params[2] eq "CA" && ($params[3] eq "ASP" || $params[3] eq "GLU")) {
+		if($params[2] eq "CG" && ($params[3] eq "ASP" || $params[3] eq "GLU")) {
             $pos = tell(FH);
             while($line = readline(FH)) {
                 @params2 = split(/\s+/, $line);
-                if ($params2[2] eq "CA" && ($params2[3] eq "ARG" || $params2[3] eq "LYS" || $params2[3] eq "HIS")) {
+                if ($params2[2] eq "CA" && ($params2[3] eq "ARG" || $params2[3] eq "LYS" || $params2[3] eq "HIS") && $params2[4] != ($params[4]+1)) {
                     $dist = ($params[5] - $params2[5])**2;
                     $dist += ($params[6] - $params2[6])**2;
                     $dist += ($params[7] - $params2[7])**2;
@@ -39,11 +39,11 @@ while ($file = readdir(DH)) {
 			}
 			seek(FH, $pos, 0);
 		}
-		if($params[2] eq "CA" && ($params[3] eq "ARG" || $params[3] eq "LYS" ||$params[3] eq "HIS")) {
+		if(($params[2] eq "C" || $params[2] eq"CZ") && ($params[3] eq "ARG" || $params[3] eq "LYS" ||$params[3] eq "HIS")) {
 			$pos = tell(FH);
 			while($line = readline(FH)) {
 				@params2 = split(/\s+/, $line);
-				if ($params2[2] eq "CA" && ($params2[3] eq "ASP" || $params2[3] eq "GLU" )) {
+				if ($params2[2] eq "CG" && ($params2[3] eq "ASP" || $params2[3] eq "GLU" ) && $params2[4] != ($params[4] +1)) {
 					$dist = ($params[5] - $params2[5])**2;
 					$dist += ($params[6] - $params2[6])**2;
 					$dist += ($params[7] - $params2[7])**2;
