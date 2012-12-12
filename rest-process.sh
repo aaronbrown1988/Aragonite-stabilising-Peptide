ed424d3162a5a9d8fb1492e3c30c4b64b10fc369
Aaron Brown <aaron@husan.co.uk>
2012-12-12 04:53:23 +0000

    Post Processing
    Minor improvements to my post processing routine.

--- rest-process.sh
+++ rest-process.sh
@@ -25,7 +25,7 @@ for i in {'0.25','0.4','0.5','0.6'}; do
 
 	echo -e "4\n1\n" |  g_cluster -f $XTC -s  $TPR -g cluster.log -clid clust-id.xvg -cl clusters.pdb -dt 10 -method gromos -cutoff $i -minstruct 1
 
-	bash ~/src/utils/cluster_post.sh 11
+	bash ~/src/utils/cluster_post.sh 
 	~/src/utils/clust_criteria.pl clust-id.xvg
 	~/src/utils/clust_time.pl clust-id.xvg
 	cat cluster.log | awk '{print $1,$3}' | sed -e 's/.*|//g' | sort -n | uniq | grep -P '^[0-9].*' >> clust_size.tsv
@@ -118,19 +118,10 @@ cd ..
 
 
 
-mkdir hbond
-cd hbond
-
-echo -e "1\n1\n" | g_hbond -f $XTC -s $TPR -dist hbdist.xvg -life hblife.xvg -ac hbac.xvg  -num hbnum.xvg -g hb.log 
-
-cd ..
-
-
-
 
 mkdir cam
 cd cam
-echo -e "1\n" | trjconv -f $XTC -s $TPR -o ./trajout.pdb -pbc whole -dt 10
+echo -e "1\n1\n" | trjconv -f $XTC -s $TPR -o ./trajout.pdb -pbc whole  -center -dt 10
 mok '{$MOL->write("$i.pdb", format=>"pdb"); $i++;}BEGIN {$i=0}' trajout.pdb 
 rm trajout.pdb
 for i in `ls *.pdb`; do
@@ -143,8 +134,18 @@ mv *.pdb.camshift *.pdb raw/
 
 cd ..
 
+mkdir hbond
+cd hbond
+
+echo -e "1\n1\n" | g_hbond -f $XTC -s $TPR -dist hbdist.xvg -life hblife.xvg -ac hbac.xvg  -num hbnum.xvg -g hb.log 
+perl ~/src/utils/hbonds.pl ../cam/raw > HB.xvg
+
+cd ..
+
+
+
 mkdir trj
 cd trj
 ~/src/utils/charge_dist.pl ../cam/raw > charge_dist.dat
-~/src/utils/ring_distance.pl ../cam/raw> ing_dist.dat
+~/src/utils/ring_distances.pl ../cam/raw> ring_dist.dat
 cd ..
