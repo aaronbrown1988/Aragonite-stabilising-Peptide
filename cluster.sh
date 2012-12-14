359d7a9e0e5abdb8b004f7baae5b23ee7b12e66c
Aaron Brown <a.h.brown@warwick.ac.uk>
2012-12-14 11:32:29 +1100

    New clustering
    Couple small mods

--- cluster.sh
+++ cluster.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+XTC=$1
+TPR=$2
+CUTOFF=$3
 steps=`gmxcheck -f $XTC 2>&1 | grep Step | awk '{print $2};'`
 time=$[$steps*10 - 10]
 end=10000;
 while [[ $end -le $time ]]; do
 	name=$[$end/1000];
 	name=$name"ns"
-	g_cluster -f $XTC -s $TPR -method gromos -e $end -o $name.log -dist $name-dist.xvg -clid $name-id.xvg -cl $name.pdb 2>&1 > $name-my.log
+	echo -e "4\n1\n" | g_cluster -f $XTC -s $TPR -method gromos -e $end -o $name.log -dist $name-dist.xvg -clid $name-id.xvg -cutoff $CUTOFF -cl $name.pdb 2>&1 > $name-my.log
 	n=`fgrep Found $name.log | awk ' {print $2};'`
 	echo "$end $n" >> clust_time.tsv
 done
