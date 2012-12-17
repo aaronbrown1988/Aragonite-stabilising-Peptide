923ec822211ec6f9bcd49bfb5007876990a68ce8
Aaron Brown <aaron@husan.co.uk>
2012-12-17 00:45:16 +0000

    17/12/12

--- cluster.sh
+++ cluster.sh
@@ -8,7 +8,7 @@ end=10000;
 while [[ $end -le $time ]]; do
 	name=$[$end/1000];
 	name=$name"ns"
-	echo -e "4\n1\n" | g_cluster -f $XTC -s $TPR -method gromos -e $end -o $name.log -dist $name-dist.xvg -clid $name-id.xvg -cutoff $CUTOFF -cl $name.pdb 2>&1 > $name-my.log
+	echo -e "4\n1\n" | g_cluster -f $XTC -s $TPR -method gromos -e $end -o $name-rmsd-clust.xpm -g $name.log -dist $name-dist.xvg -clid $name-id.xvg -cutoff $CUTOFF -cl $name.pdb 2>&1 > $name-my.log
 	n=`fgrep Found $name.log | awk ' {print $2};'`
 	echo "$end $n" >> clust_time.tsv
 	end=$[$end+10000];
