af27b8b9f0863b0f683b3c504d04b1188d69587d
Aaron Brown <a.h.brown@warwick.ac.uk>
2014-05-27 17:34:42 +1000

    int to LONG LONG
    fixed natural bottle neck in number of points that could be analysed.

--- r_anal.cpp
+++ r_anal.cpp
@@ -5,6 +5,11 @@
 #include <cstring>
 using namespace std;
 
+//
+// AHB: MODIFIED TO USE LONG LONG INTS FOR COUNTING
+// FIXED BOTTLE NECK OF INTS USED IN FOR LOOPS
+
+
 int main(int argc, char *argv[])
 {
   cout << "To use this program you need to have entered ./program_name file_name"<<endl;
@@ -14,41 +19,53 @@ int main(int argc, char *argv[])
     cout << "program requires command line file which is the output from g_rama with the text at teh top of the file removed\n";
     return 1;}
 
-  // int num =200;
-  long num;
+  // long long num =200;
+  long long num;
   char buffer[256];
-  double phi[200000];
-  double psi[200000];
-  char str[200000][8];
-
+ //double phi[500000];
+  //double psi[500000];
+  double *phi;
+  double *psi;
+  char str[100];
+  
   cout <<endl;
   cout<<endl;
   cout<<"Enter number of lines in your g_rama output file (the one without the text lines at the top):"<<endl;
   cin >> num;
 
+  phi = (double *)malloc(sizeof(double)*num);	  
+  psi = (double *) malloc(sizeof(double)*num);	  
+
+  if (phi == NULL || psi == NULL) {
+	  cout << "Couldn't get memory";
+	  cout << endl;
+	  return 1;
+  }
+  
   ifstream in1(argv[1], ios::in | ios::binary);
   if(!in1) {
     cout << "Cannot open input file\n";
     return 1;}
 
-  for(int i=0; i<num; i++){
-    in1 >>phi[i]>>psi[i]>>str[i];
+  for(long long i=0; i<num; i++){
+    in1 >>phi[i]>>psi[i]>>str;
     in1.getline(buffer,256);
   }
   in1.close();
 
   double *hi;
   double *si;
-  long *count;
-  count = (long*) malloc (sizeof(long)*num);
+  double *count;
+  
+  count = (double*) malloc (sizeof(double)*num);
   hi = (double*) malloc (sizeof(double)*num);
   si = (double*) malloc (sizeof(double)*num);
 
-  static long c=0;
+  static long long c=0;
 
-  for (int i=0; i<num; i++){
+  for (long long i=0; i<num; i++){
     if(phi[i] != 200 && psi[i] != 200){
-      for(int j=i+1; j<num; j++){
+      for(long long j=i+1; j<num; j++){
 	if((phi[i] <= (phi[j]+0.50) && phi[i] >= (phi[j]-0.50)) && (psi[i] <=(psi[j]+0.50) && psi[i] >= (psi[j]-0.50))){
 	  //cout << "found a match"<<endl;
 	count[c]= count[c] +1;
@@ -73,45 +90,45 @@ int main(int argc, char *argv[])
     out << "@target G0.S0"<<endl;
     out << "@type xy"<<endl;
 
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i]<2){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}
   out << "&"<<endl;
   out << "@target G0.S1"<<endl;
   out << "@type xy"<<endl;
 
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i]<5 && count[i] >=2){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}
 out << "&"<<endl;
  out << "@target G0.S2"<<endl;
   out << "@type xy"<<endl;
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i]<10 && count[i] >=5){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}	
 out << "&"<<endl;
  out << "@target G0.S3"<<endl;
   out << "@type xy"<<endl;
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i]<15 && count[i] >= 10){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}
 out << "&"<<endl;
  out << "@target G0.S4"<<endl;
   out << "@type xy"<<endl;
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
      if(count[i]<20 && count[i] >= 15){
        out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}
 out << "&"<<endl;
  out << "@target G0.S5"<<endl;
   out << "@type xy"<<endl;
 
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i]<30 && count[i] >= 20){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}}
 out << "&"<<endl;
   out << "@target G0.S6"<<endl;
   out << "@type xy"<<endl;
-  for(int i=0; i<c; i++) {
+  for(long long i=0; i<c; i++) {
     if(count[i] >= 30){
       out<<setw(5)<<setprecision(4)<<setw(10)<<hi[i]<<setw(10)<<si[i]<<endl;}
  }  
