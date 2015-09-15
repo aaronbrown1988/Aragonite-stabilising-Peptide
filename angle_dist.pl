babc9fff8f09e3391a336f5369d6d1e03ee66373
Aaron Brown <a.h.brown@warwick.ac.uk>
2015-09-15 14:31:17 +1000

    water angle\n do an entire folder rather than just one

--- angle_dist.pl
+++ angle_dist.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 #
-# Takes a PDB and analyses the angle of the
+# Takes a folder of PDB and analyses the angle of the
 # the waters to see how 'random' they are
 #
+
+#USage angle_dist.pl folder last non-water start end dimension
 
 #use Math::Vec;
 use Math::Vector::Real;
@@ -13,70 +15,93 @@ $axis = $ARGV[4] + 4;
 
 print "# going from $zlo to $zhi in $axis\n";
 
-$str = 0.1;
+$str = 0.5;
 $box = 5;
 
-# ARGS pdb slab_end 
-open(PDB, "$ARGV[0]");
-$line = readline(PDB);
 for ($i = 0; $i < $zhi/$str; $i++) {
 	$adr[$i] = 0;
 	$num[$i] = 0;
 }
 
-while(!eof(PDB)) {
-	# Move to water section
-	while ($line = readline(PDB)) {
-		@params = split(/\s+/, $line);
-		if ($params[1] == $ARGV[1]) {
-			last;
-		}
+opendir(DH, "$ARGV[0]");
+$nfiles = 0;
+while ($file = readdir(DH)) {
+	if ($file !~/[0-9]+\.pdb\b/) {
+		next;
 	}
-	$l = 0;
+	open(PDB, "$ARGV[0]/$file");
+	$nfiles++;
+	process();
+	close(PDB);
+	print STDERR "\b" x1000; 
+	print STDERR "$nfiles processed";
 
-	while ($line = readline(PDB)) {
-		if($line =~ /END.*/) {
-			last;
-		}
-		$line =~ s/X1/X 1/;
-		@lines[$l] = $line;
-		$l++;
-	}
-	for ($i = 0; ($zlo +$i *$str) < $zhi; $i++) {
-		$z = $zlo + $i*$str;
-	#	print "$z:\n";
-		for ($j = 0; $j < $l; $j += 3) {
-			@params = split(/\s+/, $lines[$j]);
-			if ($params[$axis] < ($zlo +$i*$str) || $params[$axis] > ($zlo +$i*$str+$box)) {
-				next;
-			}
-		#	print $Ov,"\n";
-			$Ov = V($params[5], $params[6],$params[7]);
-			@params = split(/\s+/, $lines[$j+1]);
-			$H1v = V($params[5], $params[6],$params[7]);
-			@params = split(/\s+/, $lines[$j+2]);
-			$H2v = V($params[5], $params[6],$params[7]);
-	
-			$a = 0.5*($H2v - $H1v);
-			$a = $a + $H1v;
-			$b = $a - $Ov;
-			#$n = $a x $b;
-	
-			$dot = $b * V(0,0,1);
-			$dot = $dot/ abs($b);
-		#	print "$i\t$dot\n";
-			$adr[$i] += $dot;
-			$num[$i] ++;
-		}
-	}
 }
-	
+
 for ($i = 0; $i < ($zhi/$str); $i++) {
 #	print "$i\n";
 	if ($num[$i] != 0) {
 	#	print "$adr[$i]\n";
-		$adr[$i] = $adr[$i] / $num[$i];
+		$adr[$i] = $adr[$i] / ($num[$i]);
 		$z = $zlo + $i*$str;
 		print "$z\t$adr[$i]\t$num[$i]\n";
 	}
+
 }
+
+
+
+
+sub process {
+# ARGS pdb slab_end 
+	$line = readline(PDB);
+	
+	while(!eof(PDB)) {
+		# Move to water section
+		while ($line = readline(PDB)) {
+			@params = split(/\s+/, $line);
+			if ($params[1] == $ARGV[1]) {
+				last;
+			}
+		}
+		$l = 0;
+			
+		while ($line = readline(PDB)) {
+			if($line =~ /END.*/) {
+				last;
+			}
+			$line =~ s/X1/X 1/;
+			@lines[$l] = $line;
+			$l++;
+		}
+		for ($i = 0; ($zlo +$i *$str) < $zhi; $i++) {
+			$z = $zlo + $i*$str;
+		#	print "$z:\n";
+			for ($j = 0; $j < $l; $j += 3) {
+				@params = split(/\s+/, $lines[$j]);
+				if ($params[$axis] < ($zlo +$i*$str) || $params[$axis] > ($zlo +$i*$str+$box)) {
+					next;
+				}
+			#	print $Ov,"\n";
+				$Ov = V($params[5], $params[6],$params[7]);
+				@params = split(/\s+/, $lines[$j+1]);
+				$H1v = V($params[5], $params[6],$params[7]);
+				@params = split(/\s+/, $lines[$j+2]);
+				$H2v = V($params[5], $params[6],$params[7]);
+		
+				$a = 0.5*($H2v - $H1v);
+				$a = $a + $H1v;
+				$b = $a - $Ov;
+				#$n = $a x $b;
+		
+				$dot = $b * V(1,0,0);
+				$dot = $dot/ abs($b);
+			#	print "$i\t$dot\n";
+				$adr[$i] += $dot;
+				$num[$i] ++;
+			}
+		}
+	}
+	@lines = undef();
+}
+	
